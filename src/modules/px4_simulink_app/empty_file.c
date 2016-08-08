09eaa9261a359e2fb079e83b03312f524a3318af
darenlee <daren.lee@mathworks.com>
2016-08-08 09:16:04 -0400

    Empty module files

---
+++ src/modules/px4_simulink_app/empty_file.c
@@ -0,0 +1,11 @@
+
+// this is a place-holder file to complete the build process
+
+__EXPORT int px4_simulink_app_main(int argc, char *argv[]);
+
+int px4_simulink_app_main(int argc, char *argv[]) //Px4 App
+{
+ 
+  return 0;
+}
+
